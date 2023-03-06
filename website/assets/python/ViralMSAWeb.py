9a5e1199786ec9b687445e5d41b981db47dd699f
Daniel Ji <jidaniel1234@gmail.com>
2023-03-05 18:00:11 -0800

    get janky version of ViralMSA running

--- website/assets/python/ViralMSAWeb.py
+++ website/assets/python/ViralMSAWeb.py
@@ -1,10 +1,22 @@
 #! /usr/bin/env python3
 import ViralMSA
+import asyncio
+
+oldSubprocessCall = ViralMSA.subprocess.call
 
 def checkOutputOverride(args):
     return str.encode("'Usage: minimap2'")
 
+async def callOverride(command, stderr):
+    if (command[0] != 'minimap2'):
+        return oldSubprocessCall(command, stderr=stderr)
+    
+    print('hit')
+    await minimap2Override(command)
+    print('done')
+
 if ('arguments' in globals()):
     ViralMSA.sys.argv = arguments.split()
     ViralMSA.subprocess.check_output = checkOutputOverride
-    ViralMSA.main()
+    ViralMSA.subprocess.call = callOverride
+    asyncio.get_running_loop().create_task(ViralMSA.main())
