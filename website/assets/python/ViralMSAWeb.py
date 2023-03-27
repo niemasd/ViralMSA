c322907c8530bd04045be7720a501b58a200777c
Daniel Ji <jidaniel1234@gmail.com>
2023-03-27 13:39:00 -0700

    wrap async in sync, add SharedArrayBuffer, add / improve serviceworkers for minimap2, viralmsa, cross-origin isolation

--- website/assets/python/ViralMSAWeb.py
+++ website/assets/python/ViralMSAWeb.py
@@ -7,16 +7,14 @@ old_subprocess_call = ViralMSA.subprocess.call
 def check_output_override(args):
     return str.encode("'Usage: minimap2'")
 
-async def call_override(command, stderr):
+def call_override(command, stderr):
     if (command[0] != 'minimap2'):
         return old_subprocess_call(command, stderr=stderr)
     
-    await minimap2Override(command)
+    minimap2Override(command)
 
 if ('arguments' in globals()):
     ViralMSA.sys.argv = arguments.split()
     ViralMSA.subprocess.check_output = check_output_override
     ViralMSA.subprocess.call = call_override
-    main_task = asyncio.create_task(ViralMSA.main())
-    main_task.add_done_callback(ViralMSAFinish)
-    asyncio.get_running_loop().run_until_complete(main_task)
+    ViralMSA.main()
