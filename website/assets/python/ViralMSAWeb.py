3b800c35008062622dc68f3e727524b221df253f
Daniel Ji <jidaniel1234@gmail.com>
2023-03-27 14:55:02 -0700

    refactor code, send mmi through sharedarraybuffer, add documentation

--- website/assets/python/ViralMSAWeb.py
+++ website/assets/python/ViralMSAWeb.py
@@ -1,20 +1,25 @@
 #! /usr/bin/env python3
 import ViralMSA
-import asyncio
-
+ 
 old_subprocess_call = ViralMSA.subprocess.call
 
+# override to ensure that ViralMSA sanity check for minimap2 passes for web implementation
 def check_output_override(args):
     return str.encode("'Usage: minimap2'")
 
+# override minimap2 call to run Javascript adaptation instead
 def call_override(command, stderr):
     if (command[0] != 'minimap2'):
+        # run old subprocess call if not minimap2 (won't work because Pyodide doesn't support subprocess.call)
         return old_subprocess_call(command, stderr=stderr)
     
     minimap2Override(command)
 
 if ('arguments' in globals()):
+    # set command line arguments
     ViralMSA.sys.argv = arguments.split()
+    # set overrides
     ViralMSA.subprocess.check_output = check_output_override
     ViralMSA.subprocess.call = call_override
+    # run ViralMSA
     ViralMSA.main()
