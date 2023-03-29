1d2f22ce22c3aee99502e59664f65121b5bed0bb
Daniel Ji <jidaniel1234@gmail.com>
2023-03-29 00:04:53 -0700

    fix preload github action

--- actions/preload.py
+++ actions/preload.py
@@ -1,9 +1,10 @@
 #! /usr/bin/env python3
+import os
 import sys
 sys.path.append('./')
 
 import ViralMSA
 
 for key, value in ViralMSA.REFS.items():
-    ViralMSA.download_ref_genome(value, '../website/assets/indexes/' + key, '../website/assets/indexes/' + key + '/' + key + '.fas', 'email@address.com')
-    ViralMSA.build_index_minimap2('../website/assets/indexes/' + key + '/' + key + '.fas', 1)
+    ViralMSA.download_ref_genome(value, 'website/assets/indexes/' + key, 'website/assets/indexes/' + key + '/' + key + '.fas', 'email@address.com')
+    ViralMSA.build_index_minimap2('website/assets/indexes/' + key + '/' + key + '.fas', 1)
