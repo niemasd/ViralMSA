5301f7f087f2a83737426d89515e29576c170bc4
Daniel Ji <jidaniel1234@gmail.com>
2023-03-28 21:09:42 -0700

    work on preloading indexes github action, restructure webworkers, and update ViralMSA.py

---
+++ actions/preload.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+import sys
+sys.path.append('./')
+
+import ViralMSA
+
+for key, value in ViralMSA.REFS.items():
+    ViralMSA.download_ref_genome(value, '../website/assets/indexes/' + key, '../website/assets/indexes/' + key + '/' + key + '.fas', 'email@address.com')
+    ViralMSA.build_index_minimap2('../website/assets/indexes/' + key + '/' + key + '.fas', 1)
